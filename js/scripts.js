67e1ac7b5baac51be015eb19c9fdca759b1498d5
Matt Luker <jamesmattluker@gmail.com>
2021-10-21 11:47:38 -0600

    change to a select box from cards
    Co-Authored-by: Jacob Wilson <jacobleeeugenewilson@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  // $(".harpyEagle").click(function() {
+  //   $("#harpyEagleImg").toggle();
+  //   $("#harpyEagleText").toggle();
+  // })
+  // $(".jellyfish").click(function() {
+  //   $("#jellyfishImg").toggle();
+  //   $("#jellyfishText").toggle();
+  // })
+  // $(".tardigrade").click(function() {
+  //   $("#tardigradeImg").toggle();
+  //   $("#tardigradeText").toggle();
+  // })
+  // $(".elephant").click(function() {
+  //   $("#elephantImg").toggle();
+  //   $("#elephantText").toggle();
+  // })
+});
+
+"<p class ='card-text outputText'>" + selectedText + "</p>"
